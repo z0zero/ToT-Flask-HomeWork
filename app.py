9cd01a4aab22e80f6e54f3d39984606050c8f185
z0zero <briansangapta@gmail.com>
2023-12-23 23:49:01 +0700

    Versi API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import pickle
 from sklearn import preprocessing
@@ -42,6 +42,43 @@ def prediction():
         result_medv = model.predict(df_prediction)[0]
         return render_template("prediction.html", medv=result_medv, rm=rm, lstat=lstat, ptratio=ptratio)
 
+@app.route("/api/prediction", methods=["POST"])
+def api_prediction():
+    try:
+        # Proses data input
+        rm = float(request.form["RM"])
+        lstat = float(request.form["LSTAT"])
+        ptratio = float(request.form["PTRATIO"])
+
+        df_prediction = pd.DataFrame({
+            "RM": [rm],
+            "LSTAT": [lstat],
+            "PTRATIO": [ptratio]
+        })
+
+        df_prediction[['RM', 'LSTAT', 'PTRATIO']] = data_scaler.fit_transform(df_prediction[['RM', 'LSTAT', 'PTRATIO']])
+
+        # Lakukan prediksi
+        result_medv = model.predict(df_prediction)[0]
+
+        # Kembalikan hasil dalam format JSON
+        return jsonify({
+            "status": {
+                "code": 200,
+                "message": "Success predicting"
+            },
+            "data": {
+                "medv_result": result_medv
+            }
+        })
+
+    except Exception as e:
+        return jsonify({
+            "status": {
+                "code": 500,
+                "message": "Error in prediction: " + str(e)
+            }
+        })
 
 if __name__ == "__main__":
     app.run()
