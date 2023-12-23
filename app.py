7ca360e821d0e9a8afcdc9b3f5bd53590b49c67a
z0zero <briansangapta@gmail.com>
2023-12-23 19:40:31 +0700

    first commit

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import pickle
 from sklearn import preprocessing
 
 
-app = Flask(...)
+app = Flask(__name__)
 
-model = pickle.load(open("...", "rb"))
+model = pickle.load(open("linear_regression_model.pkl", "rb"))
 data_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
 
 
@@ -15,9 +15,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/prediction", methods=["..."])
+@app.route("/prediction", methods=["POST"])
 def prediction():
-    if request.method == "...":
+    if request.method == "POST":
         # Terima input features dari user dalam bentuk float
         # Pastikan request form menerima key yang sama dari atribut name
         # Yang ada di input index.html
@@ -28,7 +28,9 @@ def prediction():
         # Karena modelnya ditrain dalam bentuk Pandas DF,
         # jadi input user harus diolah juga dalam bentuk Pandas DF
         df_prediction = pd.DataFrame({
-            ...
+            "RM": [rm],
+            "LSTAT": [lstat],
+            "PTRATIO": [ptratio]
         })
         
         # Sedikit pre-processing, karena di data training datanya ditransform,
@@ -37,7 +39,7 @@ def prediction():
             df_prediction[['RM', 'LSTAT', 'PTRATIO']])
         
         # Lakukan prediksi ke target
-        result_medv = model....(df_prediction)
+        result_medv = model.predict(df_prediction)[0]
         return render_template("prediction.html", medv=result_medv, rm=rm, lstat=lstat, ptratio=ptratio)
 
 
